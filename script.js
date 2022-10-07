6e1294d40f8e2e02e5a66e2a9c6598f7072fea47
Josh Garcia <joshgarcia7410@gmail.com>
2022-10-07 13:41:35 -0700

    adds second api call

--- script.js
+++ script.js
@@ -7,8 +7,24 @@ displayTauros(data);
 function displayTauros(data){
 const sprite = data.sprites.front_default;
 const name = data.name;
+const id = data.id;
+
+console.log(id);
 console.log(name);
 console.log(sprite);
+
 document.getElementById("demo").innerHTML = `<img src=${sprite}>`;
 document.getElementById("name").innerHTML = name;
+document.getElementById("number").innerHTML = id;
+}
+
+fetch("https://pokeapi.co/api/v2/pokemon/muk").then((response) => response.json()).then(data => {
+    console.log(data);
+    displayMuk(data);
+});
+
+function displayMuk(data) {
+    const name = data.name;
+    const id = data.id;
+    const sprite = data.sprites.front_default;
 }
