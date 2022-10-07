5ea06589efb3ad9e26d2cd71fb83c27bba733ff2
Josh Garcia <joshgarcia7410@gmail.com>
2022-10-07 13:54:12 -0700

    adds muk to list

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ console.log(id);
 console.log(name);
 console.log(sprite);
 
-document.getElementById("demo").innerHTML = `<img src=${sprite}>`;
+document.getElementById("tauros").innerHTML = `<img src=${sprite}>`;
 document.getElementById("name").innerHTML = name;
-document.getElementById("number").innerHTML = id;
+document.getElementById("taurosNumber").innerHTML = id;
 }
 
 fetch("https://pokeapi.co/api/v2/pokemon/muk").then((response) => response.json()).then(data => {
@@ -27,4 +27,9 @@ function displayMuk(data) {
     const name = data.name;
     const id = data.id;
     const sprite = data.sprites.front_default;
+
+document.getElementById("muk").innerHTML = `<img src="${sprite}">`;
+document.getElementById("name2").innerHTML = name;
+document.getElementById("mukNumber").innerHTML = id; 
+
 }
